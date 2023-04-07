1aee00be017b8a74eb7dac1da68e0ed107d729ba
Viktor Chen <chenviktor87@gmail.com>
2023-04-07 16:49:32 +0500

    add Example 005 If Else Condition

---
+++ Example005_IfElseCondition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введитя имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.Write("Yes, it's Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
