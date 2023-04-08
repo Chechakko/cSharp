8dea35b4a1283b76d534933ca9ab152b2a05830c
Viktor Chen <chenviktor87@gmail.com>
2023-04-08 18:30:35 +0500

    add Example 009 Seminar

---
+++ Example009_Seminar/Program.cs
@@ -0,0 +1,23 @@
+﻿int numberA = 2;
+int numberB = 5;
+
+int min = numberA;
+int max = numberB;
+
+Console.WriteLine(min);
+Console.WriteLine(max);
+
+
+int numberA = new Random().Next(1,10); 
+Console.WriteLine(numberA);
+if
+{
+    min = numberA;
+    Console.WriteLine(numberA);
+}
+if 
+{
+    max = numberA;
+}
+int result = numberA * numberA;
+Console.WriteLine(result);
